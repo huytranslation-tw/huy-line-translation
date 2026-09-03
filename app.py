99348f75e5f11c70d24f70b7f8af7749cdb24f07
huytranslation-tw <agrithanglong@gmail.com>
2026-09-04 02:17:25 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.v3.exceptions import InvalidSignatureError
+
+app = Flask(__name__)
+
+configuration = Configuration(
+    access_token=os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+)
+
+handler = WebhookHandler(
+    os.environ["LINE_CHANNEL_SECRET"]
+)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "HUY Translation Bot is running!"
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers.get("X-Line-Signature")
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    text = event.message.text
+
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=text)]
+            )
+        )
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
